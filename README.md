3d824f19b648a9b874f6a2d8fe811fb11b40330c
Zoel Lindstrom <zoelsburgers@gmail.com>
2022-06-01 15:18:04 -0500

    added a run function and phrase storage

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-Nothing crazy
-just some JS practice to create a randomized message and get used to using GIT.
+# Nothing crazy
+### just some JS practice to create a randomized message and get used to using GIT.
+includes a run called *run* loop to test as many phrases as you'd like and then store them in an **array**.
 Not even sure I did it right this seemed way too easy.
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const verb = ['drink', 'walk', 'fart', 'lie', 'cough', 'yank'];
 const adj = ['big', 'blue', 'farty', 'ridiculous', 'goofy'];
 const noun = ['dog', 'log', 'bog', 'frog', 'hog', 'pteradactyl', 'grog', 'dinosaur', 'rocket'];
+let phrases = [];
+
+
 
 function get(arr){
     let i = (Math.floor(Math.random() * arr.length));
@@ -9,6 +12,22 @@ function get(arr){
 
 
 function message(name) {
-    console.log(`did ${name} really ${get(verb)} on the ${get(adj)} ${get(noun)}?!`)
+    let phrase = `did ${name} really ${get(verb)} on the ${get(adj)} ${get(noun)}?!`;
+    if (phrases.includes(phrase)){
+        console.log('phrase already spoken');
+    } else {
+        phrases.push(phrase);
+    }
 }
-message('Zoel');
+
+
+function run(j, name){
+    for(i = 0; i <= j; i++){
+        message(name);
+    }
+}
+
+
+
+run(100, 'Zoel');
+console.log(phrases);
